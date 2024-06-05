21e03a3e9c3e746614e63b01a8ae1041ee2a44a7
big-viktor <79458452+big-viktor@users.noreply.github.com>
2024-06-05 13:11:08 +0200

    serverPort

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const app = express();
-const port = 1000;
+const port = `1000`;
 
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
