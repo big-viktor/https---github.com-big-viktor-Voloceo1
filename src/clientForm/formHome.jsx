423fcf028fad7e4735077264c76807b40cf0284d
big-viktor <79458452+big-viktor@users.noreply.github.com>
2024-06-05 13:11:36 +0200

    port

--- src/clientForm/formHome.jsx
+++ src/clientForm/formHome.jsx
@@ -14,7 +14,7 @@ const FormHome = () => {
 
 	const sendMail = () => {
 		axios
-			.get('https://volceo0-1-git-main-nextestwork.vercel.app/', {
+			.get(' http://localhost:1000', {
 				params: {
 					email: 'svitlik.viktor78@gmail.com',
 					subject: 'FormHome',
